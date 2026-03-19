a790da67d702d1a80d1015b01303b26b1d72dd3e
Luís Teixeira <micboucinha@gmail.com>
2026-03-19 21:05:04 +0000

    Add RegisterField

---
+++ App/Harp.LedArray.Design/Controls/RegisterField.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+
+namespace Harp.LedArray.Design.Controls;
+
+public class RegisterField : HeaderedContentControl
+{
+    public static readonly StyledProperty<string?> DescriptionProperty =
+        AvaloniaProperty.Register<RegisterField, string?>(nameof(Description));
+    
+    public static readonly StyledProperty<IBrush?> IconForegroundProperty =
+        AvaloniaProperty.Register<RegisterField, IBrush?>(nameof(IconForeground));
+
+    public string? Description
+    {
+        get => GetValue(DescriptionProperty);
+        set => SetValue(DescriptionProperty, value);
+    }
+    
+    public IBrush? IconForeground
+    {
+        get => GetValue(IconForegroundProperty);
+        set => SetValue(IconForegroundProperty, value);
+    }
+}
